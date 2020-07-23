52f315eb880700a3317e146654d84e5ab2690fef
MemonaKhan <you@example.comsuhail1975pk@yahoo.co.uk>
2020-07-23 21:41:24 +0500

    created question for demo

---
+++ app.js
@@ -0,0 +1,12 @@
+var quiz = {
+    "1":{
+        question:"What is the name of country",
+        correct:"Pakistan",
+        opt1:"Pakistan",
+        opt2:"Bangladesh",
+        opt3:"Iran",
+        opt4:"China",
+        answer:"",
+    }
+}
+var quiz = document.getElementById("quiz");
